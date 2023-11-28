d9a9cd61ec7e87ce7b68c30596476810ea2e49c6
Rustam <rustamjonisroilov213@gmail.com>
2023-11-28 17:06:03 +0500

    swagger

--- apps/urls.py
+++ apps/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     # path('', include('viewset.urls')),
     # path('', include('genericview.urls')),
-    path('', include('apiview.urls')),
+    path('', include('genericview.urls')),
 ]
